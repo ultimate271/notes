0292ff408af9d4cddfb5345a6deac954226e6310
Brett Webster <bfireflash@gmail.com>
2019-05-01 22:40:55 -0500

    A commit

--- book/SethAndCain/pandoc.bat
+++ book/SethAndCain/pandoc.bat
@@ -1,15 +1,48 @@
+@echo off
+setlocal
+
+for %%I in (.) do set CurrDirName=%%~nxI
+
+echo (Starting) [Executing pandoc.bat for %CurrDirName%]
+echo                                                             ...
+
 cd src
-pandoc -o "..\bin\SethAndCain.pdf" ^
-    "001.md" ^
-    "002.md" ^
-    "003.md" ^
-    "004.md" ^
-    "005.md" ^
-    "006.md" ^
-    "007.md" ^
-    "008.md" ^
-    "..\metadata.yaml" ^
-    --template="template.latex"
+
+echo (Starting) [Retrieving Source]
+echo                                                             ...
+for %%I in (*.md) do (
+    call set "SrcList=%%SrcList%% ""%%I"""
+    echo   SRC : %%I
+)
+echo (Update) [Checking Metadata]
+if exist "metadata.yaml" (
+    call set "SrcList=%%SrcList%% ""metadata.yaml"""
+    echo   FOUND: metadata.yaml
+) else (
+    echo   MISSING: metadata.yaml
+)
+if exist "template.latex" (
+    call set "SrcList=%%SrcList%% --template=""template.latex"""
+    echo   FOUND: template.latex
+) else (
+    echo   MISSING: template.latex
+)
+
+echo (Complete) [Retrieving Source]
+echo                                                             !
+echo (Starting) [Executing pandoc]
+echo                                                             ...
+echo pandoc -o "..\bin\something.pdf" "file001.md" "file323.md" "metadata.yaml" and so on
+
+pandoc -o "..\bin\%CurrDirName%.pdf" %SrcList%
+
+echo (Complete) [Executing pandoc]
+echo                                                             !
+echo   OUT : %CurrDirName%.pdf copied to bin folder
+
 cd ..
 
+echo (Complete) [Executing pandoc.bat for %CurrDirName%]
+echo                                                             !
+endlocal
 
--- book/new/pandoc.bat
+++ book/new/pandoc.bat
@@ -3,11 +3,46 @@ setlocal
 
 for %%I in (.) do set CurrDirName=%%~nxI
 
+echo (Starting) [Executing pandoc.bat for %CurrDirName%]
+echo                                                             ...
+
 cd src
-for %%I in (*.md) do call set "SrcList=%%SrcList%% ""%%I"""
-if exist "metadata.yaml" call set "SrcList=%%SrcList%% ""metadata.yaml"""
-if exist "template.latex" call set "SrcList=%%SrcList%% --template=""template.latex"""
+
+echo (Starting) [Retrieving Source]
+echo                                                             ...
+for %%I in (*.md) do (
+    call set "SrcList=%%SrcList%% ""%%I"""
+    echo   SRC : %%I
+)
+echo (Update) [Checking Metadata]
+if exist "metadata.yaml" (
+    call set "SrcList=%%SrcList%% ""metadata.yaml"""
+    echo   FOUND: metadata.yaml
+) else (
+    echo   MISSING: metadata.yaml
+)
+if exist "template.latex" (
+    call set "SrcList=%%SrcList%% --template=""template.latex"""
+    echo   FOUND: template.latex
+) else (
+    echo   MISSING: template.latex
+)
+
+echo (Complete) [Retrieving Source]
+echo                                                             !
+echo (Starting) [Executing pandoc]
+echo                                                             ...
+echo pandoc -o "..\bin\something.pdf" "file001.md" "file323.md" "metadata.yaml" and so on
+
 pandoc -o "..\bin\%CurrDirName%.pdf" %SrcList%
 
+echo (Complete) [Executing pandoc]
+echo                                                             !
+echo   OUT : %CurrDirName%.pdf copied to bin folder
+
 cd ..
+
+echo (Complete) [Executing pandoc.bat for %CurrDirName%]
+echo                                                             !
 endlocal
+
